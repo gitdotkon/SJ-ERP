9b84827e2924abfe3c3d93b30b43bd6fb7ef912f
ZhaoRed <zhx_0626@163.com>
2015-03-22 14:43:57 +0800

    use a new talbe t_user

--- src/main/java/com/deere/model/User.java
+++ src/main/java/com/deere/model/User.java
@@ -2,8 +2,10 @@ package com.deere.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "t_user")
 public class User extends GenericModel {
 	
 	private String userName;
